2d5aa7a659cb24a037424af3dc50d11735abf7d8
Akash_Kumar <akdev63063@gmail.com>
2026-01-09 02:09:17 +0530

    Abstraction Method

---
+++ PythonFundaments3/Abstraction.py
@@ -0,0 +1,24 @@
+from abc import ABC , abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Lion(Animal):
+    def make_sound(self):
+        print("Road !")
+
+class Cat(Animal):
+    def make_sound(self):
+        print("Meow !")
+
+lion = Lion()
+lion.make_sound()
+
+cat = Cat()
+cat.make_sound()
+        
+
+    
+
+
