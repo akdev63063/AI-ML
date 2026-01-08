4ddc5e40d9f5f5e2fdc21c79b92ba8e0faa6575d
Akash_Kumar <akdev63063@gmail.com>
2026-01-09 02:19:08 +0530

    Function Override

---
+++ PythonFundaments3/Polymorphism_Functionoverride.py
@@ -0,0 +1,13 @@
+# Function Over riding 
+
+class Employee:
+    def get_designation(self):
+        print("Designation = Employee")
+
+class Teacher(Employee):
+    def get_designation(self):
+        print("Designation = Teacher")
+
+t1 = Teacher()
+t1.get_designation()
+
